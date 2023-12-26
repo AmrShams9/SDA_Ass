4d0660b33b76c05862d21681ec92da52f6dd70fc
Amr Shams <102551102+AmrShams9@users.noreply.github.com>
2023-12-26 23:28:34 +0200

    Update Orders.java

--- SDA-project/SDA-project/src/main/java/com/NotificationManagementSystem/SDAproject/CompositePattern/Orders.java
+++ SDA-project/SDA-project/src/main/java/com/NotificationManagementSystem/SDAproject/CompositePattern/Orders.java
@@ -3,6 +3,7 @@ package com.NotificationManagementSystem.SDAproject.CompositePattern;
 import com.NotificationManagementSystem.SDAproject.Customer;
 import com.NotificationManagementSystem.SDAproject.Location;
 import com.NotificationManagementSystem.SDAproject.PlaceHolder;
+import com.NotificationManagementSystem.SDAproject.Shipment;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -10,9 +11,17 @@ import java.util.ArrayList;
 
 public class Orders implements Composite, OrderType{
     private String orderID;
+    private OrderType orderType;
     private Location destination;
     private ArrayList<Composite> products;
     private PlaceHolder placeHolder;
+
+    public Orders(OrderType orderType, Location destination, Shipment shipment, String status) {
+        this.orderType = orderType;
+        this.destination = destination;
+        this.products = new ArrayList<>();
+        this.placeHolder = placeHolder;
+    }
 
     @Override
     public void getChild(int i) {
@@ -50,6 +59,11 @@ public class Orders implements Composite, OrderType{
     @Override
     public ArrayList getProductName() {
         return products;
+    }
+
+    @Override
+    public void setStatus(String newStatus) {
+
     }
     //Invoker of the Command Pattern
 }
